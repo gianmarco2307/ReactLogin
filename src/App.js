8a1d53c3d7ddff19acd8053a234d9015580b69bb
gian <gianmarco.clu23@gmail.com>
2024-01-24 09:49:50 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function saveUsers(users) {
+  localStorage.setItem('users', JSON.stringify(users));
+}
+
+function LoginForm(email) {
+  return (
+    <form>
+      <input type='email' id='inputEmail'></input>
+      <input type='submit' id='submitLogin' value='Login' onClick={
+        e => {
+          e.preventDefault();
+          let auth = document.getElementById('inputEmail');
+          email = auth;
+        }
+      }></input>
+    </form>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let storedEmail = JSON.parse(localStorage.getItem('authentication')) || '';
+  let users = JSON.parse(localStorage.getItem('users')) || [];
+  if(!!email) {
+    return <h1>Pagina principale</h1>
+  } else {
+    return <LoginForm></LoginForm>
+  }
 }
 
 export default App;
